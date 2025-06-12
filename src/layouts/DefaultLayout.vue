5606cee8572898662ecc463a07402425af8f0964
Mongkhon2027 <mongkhon.T650@gmail.com>
2025-06-12 15:28:29 +0700

    📝 Blank&DefaultLayout.vue

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-title>Application Bar</v-app-bar-title>
+        </v-app-bar>
+        <v-main>
+            <router-view />
+        </v-main>  
+        <v-footer color="purple">Footer</v-footer>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+
+</style>
