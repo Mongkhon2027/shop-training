b39fd1ec473a3c75fd8bee514d1c3e49dfae7487
Mongkhon2027 <mongkhon.T650@gmail.com>
2025-06-12 14:01:50 +0700

    setup shop-training

---
+++ src/views/HomeView.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- <div class="test-variable text-h4">test-variable</div> -->
+  <div class="test-variable text-lg-h4">test-variable</div>
+  <div class="test-mixin">test-mixin</div>
+    <v-card
+    class="mx-auto text-white"
+    color="#26c6da"
+    max-width="400"
+    prepend-icon="mdi-twitter"
+    title="Twitter"
+  >
+    <template v-slot:prepend>
+      <v-icon size="x-large"></v-icon>
+    </template>
+
+    <v-card-text class="text-h5 py-2">
+      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+    </v-card-text>
+
+    <v-card-actions>
+      <v-list-item class="w-100">
+        <template v-slot:prepend>
+          <v-avatar
+            color="grey-darken-3"
+            image="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-avatar>
+        </template>
+
+        <v-list-item-title>Evan You</v-list-item-title>
+
+        <v-list-item-subtitle>Vue Creator</v-list-item-subtitle>
+
+        <template v-slot:append>
+          <div class="justify-self-end">
+            <v-icon class="me-1" icon="mdi-heart"></v-icon>
+            <span class="subheading me-2">256</span>
+            <span class="me-1">·</span>
+            <v-icon class="me-1" icon="mdi-share-variant"></v-icon>
+            <span class="subheading">45</span>
+          </div>
+        </template>
+      </v-list-item>
+    </v-card-actions>
+  </v-card>
+</template>
+
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+  .test-variable {
+    background-color: $error;
+  }
+  .test-mixin {
+    @include gradient($primary, $error)
+  }
+</style>
