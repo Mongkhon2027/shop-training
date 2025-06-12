f855b671e4d091c411ec0da7c3aadcd96a959825
Mongkhon2027 <mongkhon.T650@gmail.com>
2025-06-12 15:26:10 +0700

    🎨 router/index.ts

--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -1,19 +1,71 @@
+// import path = require('path')
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: () => import('@/views/HomeView.vue'),
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('@/views/AboutView.vue'),
+    // },
     {
       path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue'),
+      redirect: '/login'
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/views/AboutView.vue'),
+      path: '/',
+      component: () => import('@/layouts/BlankLayout.vue'),
+      children :[
+        {
+          path: 'login',
+          name: 'Login',
+          component: () => import('@/views/AboutView.vue'), // Change path to Login Page
+        },
+      ]
+    },
+    {
+      path: '/default',
+      component: () => import('@/layouts/DefaultLayout.vue'),
+      meta: { requireAuth: true },
+      children :[
+        {
+          path: 'home',
+          name: 'Home',
+          component: () => import('@/views/HomeView.vue'),
+        },
+        {
+          path: 'product',
+          name: 'Product',
+          component: () => import('@/views/ProductDetailView.vue'),
+        },
+        {
+          path: 'cart',
+          name: 'Cart',
+          component: () => import('@/views/CartView.vue'),
+        },
+      ]
     },
   ],
+})
+
+const isLogin = true;
+
+router.beforeEach((to, form, next) => {
+  if (
+    to.name !== 'Login' && 
+    to.meta?.requireAuth &&
+    !isLogin
+  ) {
+    next({ name: 'LoginPage' })
+  } else {
+    next()
+  }
 })
 
 export default router
